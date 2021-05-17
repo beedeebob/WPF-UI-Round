3a733bc6b033fa6c4f436097a1ec06dcc1bca7bb
Benjamin Daniels <ben@mhe.co.za>
2021-05-17 14:18:45 +0200

    Modified the textbox and datepicker colouring. Added zoomable UI for easier viewing of controls.

--- WPF UI Green Round/MainWindow.xaml.cs
+++ WPF UI Green Round/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace WPF_UI_ROUND
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool ctrlLheld;
+        bool ctrlRheld;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            scrollViewer.PreviewMouseWheel += ScrollViewer_PreviewMouseWheel;
+            this.KeyDown += MainWindow_KeyDown;
+            this.KeyUp += MainWindow_KeyUp;
+        }
+
+        private void MainWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.LeftCtrl)
+            {
+                ctrlLheld = false;
+            }
+            if (e.Key == Key.RightCtrl)
+            {
+                ctrlRheld = false;
+            }
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.LeftCtrl)
+            {
+                ctrlLheld = true;
+            }
+            if (e.Key == Key.RightCtrl)
+            {
+                ctrlRheld = true;
+            }
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!ctrlLheld && !ctrlRheld)
+                return;
+
+            if (e.Delta > 0)
+            {
+                scaleTransform.ScaleX += 0.1;
+                scaleTransform.ScaleY += 0.1;
+            }
+            if (e.Delta < 0)
+            {
+                scaleTransform.ScaleX -= 0.1;
+                scaleTransform.ScaleY -= 0.1;
+            }
+
+            var centerOfViewport = new Point(scrollViewer.ViewportWidth / 2,
+                                             scrollViewer.ViewportHeight / 2);
+            scrollViewer.TranslatePoint(centerOfViewport, grid);
+
+            e.Handled = true;
         }
     }
 }
